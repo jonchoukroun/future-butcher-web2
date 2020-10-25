d9213a137cd1988bc49ccf3662560aa5a12fec88
Jon <jonchoukroun@gmail.com>
2020-10-25 12:09:42 -0700

    Configure eslint for build and linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "./src/index.ts",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
     devtool: "inline-source-map",
     devServer: {
         contentBase: path.join(__dirname, "dist"),
@@ -13,12 +18,29 @@ module.exports = {
         open: true,
     },
     plugins: [
+        new ESLintWebpackPlugin({
+            files: "src",
+            extensions: [".ts", ".tsx", ".js", ".jsx"],
+            failOnError: true,
+            emitWarning: true,
+            emitError: true,
+        }),
         new HtmlWebpackPlugin({
             template: "src/index.html",
         }),
     ],
     module: {
         rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)?$/,
+                use: {
+                    loader: "ts-loader",
+                    options: {
+                        configFile: path.resolve("./tsconfig.json"),
+                    },
+                },
+                exclude: /node_modules/,
+            },
             {
                 test: /.scss$/,
                 use: [
@@ -29,8 +51,7 @@ module.exports = {
             },
         ],
     },
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
+    resolve: {
+        extensions: [".tsx", ".ts", ".jsx", ".js"],
     },
 };
