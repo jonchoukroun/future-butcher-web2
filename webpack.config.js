65cd63485cf19abd3ceb2c03ee574d57bf4f4b89
Jon <jonchoukroun@gmail.com>
2020-10-25 09:21:24 -0700

    Update webpack config for dev env

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,15 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    mode: "development",
     entry: "./src/index.js",
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 3000,
+        open: true,
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "src/index.html",
