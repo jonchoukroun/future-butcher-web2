da5761d37cd0b8c166fd1612d58cc2bc818be7fb
Jon <jonchoukroun@gmail.com>
2020-11-03 16:46:42 -0800

    Add jest testing; add rendering tests for market and subway

---
+++ src/Fixtures/subwayStations.ts
@@ -0,0 +1,38 @@
+export const SUBWAY_STATIONS = [
+    {
+        name: "beverly hills",
+        travelTime: 3,
+        description: "Home of the rich & famous. Safest neighborhood.",
+        baseCrimeRate: 1,
+    },
+    {
+        name: "downtown",
+        travelTime: 2,
+        description: "Tight security.",
+        baseCrimeRate: 2,
+    },
+    {
+        name: "venice beach",
+        travelTime: 2,
+        description: "Relatively safe, but the palm trees are dead.",
+        baseCrimeRate: 3,
+    },
+    {
+        name: "hollywood",
+        travelTime: 1,
+        description: "Rough, grimy, seedy.",
+        baseCrimeRate: 4,
+    },
+    {
+        name: "compton",
+        travelTime: 1,
+        description: "Anarchy - free subway rides and certain death.",
+        baseCrimeRate: 5,
+    },
+    {
+        name: "bell gardens",
+        travelTime: 2,
+        description: "Find Gus's Army Surplus Store here.",
+        baseCrimeRate: 0,
+    },
+];
