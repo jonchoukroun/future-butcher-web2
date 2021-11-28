c382a174e0fe2afe4e022cde600fd76a93a1d3e3
Jon <jonchoukroun@gmail.com>
2021-11-27 18:41:35 -0800

    Update styles; consolidate state management

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,8 +1,7 @@
 /** @jsx jsx */
 import { Global, css, jsx } from "@emotion/react";
 
-import { PlayerProvider } from "./Player/PlayerContext";
-import { Welcome } from "./Welcome/Welcome";
+import { GameStateProvider } from "./GameState/GameStateProvider";
 import { Window } from "./Window/Window";
 import { WindowSizeProvider } from "./Window/WindowSizeProvider";
 import * as Colors from "../Styles/colors";
@@ -10,29 +9,36 @@ import * as Colors from "../Styles/colors";
 export const App = () => {
     return (
         <WindowSizeProvider>
-            <Global
-                styles={css`
-                    @import url("https://fonts.googleapis.com/css2?family=Mr+Dafoe&display=swap");
-                    body {
-                        margin: 0;
-                        background: ${Colors.Background.page};
-                    }
-                    h1,
-                    h2,
-                    h3,
-                    h4,
-                    h5,
-                    h6,
-                    p {
-                        font-family: Trebuchet MS, sans-serif;
-                    }
-                `}
-            />
-            <PlayerProvider>
-                <Window title="Future Butcher">
-                    <Welcome />
-                </Window>
-            </PlayerProvider>
+            <Global styles={GlobalStyles} />
+            <GameStateProvider>
+                <Window />
+            </GameStateProvider>
         </WindowSizeProvider>
     );
 };
+
+const GlobalStyles = css`
+    @import url("https://fonts.googleapis.com/css2?family=Mr+Dafoe&display=swap");
+
+    body {
+        margin: 0;
+        background: ${Colors.Background.page};
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p {
+        font-family: Trebuchet MS, sans-serif;
+    }
+
+    * {
+        box-sizing: border-box;
+        &:focus-visible {
+            outline: black dotted 1px;
+        }
+    }
+`;
