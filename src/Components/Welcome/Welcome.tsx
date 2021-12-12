50c0f83e3bdcc74e7031071468ffd0fbbc589e8f
Jon <jonchoukroun@gmail.com>
2021-12-12 15:30:25 -0800

    Draft welcome screen

--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -2,10 +2,11 @@
 import { jsx } from "@emotion/react";
 
 import { useWindowSize } from "../Window/WindowSizeProvider";
+import { ButtonPrimary } from "../Form";
 
 export const Welcome = () => {
-    const { getContentSize } = useWindowSize();
-    const { blockSize } = getContentSize();
+    const { getContentSize, layout } = useWindowSize();
+    const { blockSize, inlineSize } = getContentSize();
 
     const date = new Date();
     date.setFullYear(2055, date.getMonth(), date.getDate());
@@ -16,10 +17,17 @@ export const Welcome = () => {
     };
     const displayDate = date.toLocaleDateString("en-US", options);
 
+    const handleStartClick = () => {
+        console.log("!!push start game");
+        console.log("!!dispatch incrementProcess");
+        console.log("!!dispatch changeScreen: main");
+    };
+
     return (
         <div
             css={{
                 blockSize: `${blockSize}px`,
+                inlineSize: "100%",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
@@ -27,7 +35,7 @@ export const Welcome = () => {
         >
             <div
                 css={{
-                    inlineSize: "100%",
+                    inlineSize: `${inlineSize}px`,
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center",
@@ -50,12 +58,57 @@ export const Welcome = () => {
 
             <div
                 css={{
-                    inlineSize: "100%",
+                    inlineSize: `${inlineSize}px`,
                     display: "flex",
                     flexDirection: "column",
+                    alignItems: "center",
+                    marginBlockStart: layout === "full" ? "40px" : 0,
+                    paddingInline: "5px",
+                    p: {
+                        marginBlockEnd: layout === "full" ? "16px" : 0,
+                    },
                 }}
             >
-                <p>The economy is ruins after the collapse of HypeCoin.</p>
+                <p>
+                    The economy is ruins after the collapse of HypeCoin. The
+                    rich and famous are nuts for the latest fad -{" "}
+                    <em>fresh, human meat</em>.
+                </p>
+
+                <p>
+                    Two-bit hustlers like you have only one option: get rich off
+                    this new addiction, or become the product.
+                </p>
+
+                <p>
+                    Travel the city&apos;s neighborhoods and seize opportunity.
+                    But watch your back! Gangs run the city, and you don&apos;t
+                    want to cross them.
+                </p>
+
+                <p>
+                    You have 24 hours to hustle. Get a loan from the bank and
+                    hit the markets.
+                </p>
+            </div>
+
+            <div
+                css={{
+                    position: layout === "compact" ? "absolute" : "relative",
+                    bottom: 0,
+                    blockSize: "70px",
+                    inlineSize: `${inlineSize}px`,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginBlockStart: layout === "full" ? "40px" : 0,
+                }}
+            >
+                <ButtonPrimary
+                    label={"Start Game"}
+                    type={"Stretch"}
+                    clickCB={handleStartClick}
+                />
             </div>
         </div>
     );
