82051b5212877c72b8b59683ef11694a9d5bc639
Jon <jonchoukroun@gmail.com>
2021-12-17 22:29:33 -0800

    Fix time formatting

--- src/Components/Utils/getTimeLeft.ts
+++ src/Components/Utils/getTimeLeft.ts
@@ -1,6 +1,6 @@
 export function getTimeLeft(turnsLeft: number): string {
     const currentTurn = 24 - turnsLeft;
-    const time = (currentTurn % 12) + 5;
+    const time = (5 + currentTurn) % 12;
     const hours = time === 0 ? 12 : time;
 
     const unit = currentTurn >= 7 && currentTurn <= 18 ? "pm" : "am";
