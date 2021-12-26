55a59b92a162a9660c8e1605091d868d17b17f01
Jon <jonchoukroun@gmail.com>
2021-12-26 17:43:05 -0500

    Draft welcome text and category buttons

--- src/Components/SurplusStore/SurplusStore.tsx
+++ src/Components/SurplusStore/SurplusStore.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
 
+import { ButtonPrimary } from "../Form";
 import { useWindowSize } from "../Window/WindowSizeProvider";
 import * as Colors from "../../Styles/colors";
 
@@ -30,6 +31,50 @@ export const SurplusStore = () => {
                     Gus&apos; Army Surplus
                 </h2>
             )}
+
+            <p css={{ marginBlock: "40px" }}>
+                Welcome to the last bastion of freedom in LA! We have the best
+                gear to ensure your liberty.
+            </p>
+
+            <p>You look like you could use more carrying space.</p>
+            <div
+                css={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginBlockEnd: "40px",
+                }}
+            >
+                <ButtonPrimary
+                    type={"Block"}
+                    label={"Buy Packs"}
+                    scheme={"Inverse"}
+                    clickCB={() => {
+                        return;
+                    }}
+                />
+            </div>
+
+            <p>
+                Ain&apos;t no more 2nd ammendment, but we still have all your
+                personal defense needs.
+            </p>
+            <div
+                css={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginBlockEnd: "40px",
+                }}
+            >
+                <ButtonPrimary
+                    type={"Block"}
+                    label={"Buy Weapons"}
+                    scheme={"Inverse"}
+                    clickCB={() => {
+                        return;
+                    }}
+                />
+            </div>
         </div>
     );
 };
