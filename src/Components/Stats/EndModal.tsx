4f8bf1b8f8cc6b424f33cf0c27117af1c3e24d08
Jon <jonchoukroun@gmail.com>
2021-12-26 17:26:15 -0500

    Confirm end game with modal

---
+++ src/Components/Stats/EndModal.tsx
@@ -0,0 +1,105 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+import { useEffect, useRef, useState } from "react";
+import { unstable_batchedUpdates } from "react-dom";
+
+import { ButtonPrimary } from "../Form";
+import { useWindowSize } from "../Window/WindowSizeProvider";
+import { useChannel } from "../../PhoenixChannel/ChannelProvider";
+import { Screen, useGameState } from "../../GameData/GameStateProvider";
+import * as Colors from "../../Styles/colors";
+
+export const EndModal = ({ onCloseModal }: { onCloseModal: () => void }) => {
+    const isMountedRef = useRef(false);
+    useEffect(() => {
+        isMountedRef.current = true;
+        return () => {
+            isMountedRef.current = false;
+        };
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { dispatch } = useGameState();
+    const { handleEndGame } = useChannel();
+    const handleEndGameClick = async () => {
+        if (isLoading) return;
+
+        setIsLoading(true);
+        const hashId = localStorage.getItem("playerHash");
+        if (!hashId) {
+            throw new Error("Cannot end game without a hash ID");
+        }
+        const highScores = await handleEndGame(hashId, 0);
+        if (highScores === undefined) {
+            dispatch({ type: "changeScreen", screen: Screen.Error });
+            return;
+        }
+
+        unstable_batchedUpdates(() => {
+            dispatch({ type: "setHighScores", highScores });
+            dispatch({ type: "changeScreen", screen: Screen.HighScores });
+        });
+        if (isMountedRef.current) {
+            setIsLoading(false);
+        }
+    };
+
+    const { getContentSize, layout } = useWindowSize();
+    const { inlineSize } = getContentSize();
+    const inlineSizeOffset = layout === "full" ? 24 : 15;
+
+    return (
+        <div
+            css={{
+                position: "fixed",
+                insetBlockStart: 0,
+                insetBlockEnd: 0,
+                insetInlineStart: 0,
+                insetInlineEnd: 0,
+                backgroundColor: "rgba(0, 0, 0, 0.4)",
+                zIndex: 1000,
+            }}
+        >
+            <div
+                css={{
+                    position: "absolute",
+                    insetBlockStart: layout === "full" ? "70px" : "30px",
+                    insetInlineEnd: layout === "full" ? "23px" : "15px",
+                    inlineSize: `${inlineSize - inlineSizeOffset}px`,
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: "10px",
+                    backgroundColor: Colors.Background.base,
+                    borderColor: Colors.Border.subtle,
+                    borderRadius: "4px",
+                    borderStyle: "outset",
+                    borderWidth: "2px",
+                    boxShadow: "2px 2px 12px 4px rgba(0, 0, 0, 0.4)",
+                    zIndex: 1001,
+                }}
+            >
+                <h4 css={{ marginBlockEnd: "40px" }}>
+                    Are you sure you want to quit?
+                </h4>
+
+                <div css={{ display: "flex", justifyContent: "space-between" }}>
+                    <ButtonPrimary
+                        type={"Half"}
+                        label={"Cancel"}
+                        border={"None"}
+                        scheme={"Normal"}
+                        clickCB={onCloseModal}
+                    />
+
+                    <ButtonPrimary
+                        type={"Full"}
+                        label={"Quit"}
+                        isDanger={true}
+                        clickCB={handleEndGameClick}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/Components/Stats/StatsScreen.tsx
+++ src/Components/Stats/StatsScreen.tsx
@@ -2,15 +2,15 @@
 import { jsx } from "@emotion/react";
 import { faClock, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useRef, useState } from "react";
-import { unstable_batchedUpdates } from "react-dom";
+import { Fragment, useEffect, useRef, useState } from "react";
 
+import { EndModal } from "./EndModal";
 import { ButtonPrimary } from "../Form/ButtonPrimary";
 import { formatMoney, getTimeLeft } from "../Utils";
 import { useWindowSize } from "../Window/WindowSizeProvider";
 import { subwayStations } from "../../Fixtures/subwayStations";
 import { useChannel, Callback } from "../../PhoenixChannel/ChannelProvider";
-import { useGameState, Screen } from "../../GameData/GameStateProvider";
+import { useGameState } from "../../GameData/GameStateProvider";
 import * as Colors from "../../Styles/colors";
 
 export const StatsScreen = () => {
@@ -47,29 +47,17 @@ export const StatsScreen = () => {
         ([, amount1], [, amount2]) => amount2 - amount1,
     );
 
-    const { handleEndGame, handlePushCallback } = useChannel();
-    const handleEndGameClick = async () => {
-        if (isLoading) return;
-
-        setIsLoading(true);
-        const hashId = localStorage.getItem("playerHash");
-        if (!hashId) {
-            throw new Error("Cannot end game without a hash ID");
-        }
-        const highScores = await handleEndGame(hashId, 0);
-        if (highScores === undefined) {
-            dispatch({ type: "changeScreen", screen: Screen.Error });
-            return;
-        }
-
-        unstable_batchedUpdates(() => {
-            dispatch({ type: "setHighScores", highScores });
-            dispatch({ type: "changeScreen", screen: Screen.HighScores });
-        });
-        if (isMountedRef.current) {
-            setIsLoading(false);
-        }
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const handleConfirmEndGame = () => {
+        if (isModalOpen) return;
+        setIsModalOpen(true);
     };
+    const handleCancelEndGame = () => {
+        if (!isModalOpen) return;
+        setIsModalOpen(false);
+    };
+
+    const { handlePushCallback } = useChannel();
 
     const canAfford = player?.funds > player?.debt;
     const handlePayDebtClick = async () => {
@@ -91,227 +79,233 @@ export const StatsScreen = () => {
     const paddingInline = layout === "full" ? "28px" : "7px";
 
     return (
-        <div
-            css={{
-                blockSize: "calc(100% - 70px)",
-                inlineSize: "100%",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "space-evenly",
-                paddingInline: containerPaddingInline,
-                "& p": {
-                    fontFamily: "Share Tech Mono",
-                    wordSpacing: 0,
-                },
-            }}
-        >
+        <Fragment>
             <div
                 css={{
-                    inlineSize: "100%",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    paddingBlock,
-                    paddingInline,
-                    borderColor: "transparent",
-                    borderBlockEndColor: Colors.Border.subtle,
-                    borderRadius: "1px",
-                    borderStyle: "solid",
-                    borderWidth: "1px",
-
-                    "& button": {
-                        marginBlockStart: layout === "full" ? "-10px" : "-2px",
-                    },
-                }}
-            >
-                <div>
-                    <p css={{ marginBlockStart: 0, marginBlockEnd: "5px" }}>
-                        <span css={{ marginInlineEnd: "5px" }}>
-                            <FontAwesomeIcon icon={faClock} />
-                        </span>
-                        {turnsLeft && getTimeLeft(turnsLeft)}
-                    </p>
-                    <p css={{ marginBlock: 0, color: Colors.Text.subtle }}>
-                        {turnsLeft} hours left
-                    </p>
-                </div>
-                <ButtonPrimary
-                    type={"Sized"}
-                    label={"End Game"}
-                    scheme={"Inverse"}
-                    border={"None"}
-                    isDanger={true}
-                    clickCB={handleEndGameClick}
-                />
-            </div>
-
-            <div
-                css={{
-                    inlineSize: "100%",
-                    paddingBlock,
-                    paddingInline,
-                    borderColor: "transparent",
-                    borderBlockEndColor: Colors.Border.subtle,
-                    borderRadius: "1px",
-                    borderStyle: "solid",
-                    borderWidth: "1px",
-                }}
-            >
-                <div
-                    css={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        marginBlockEnd: "5px",
-                    }}
-                >
-                    <p css={{ marginBlockStart: 0, marginBlockEnd: "5px" }}>
-                        {player && formatMoney(player.funds)}
-                    </p>
-
-                    <p css={{ marginBlock: 0 }}>
-                        <span css={{ marginInlineEnd: "5px" }}>
-                            <FontAwesomeIcon icon={faHeart} fontSize={16} />
-                        </span>
-                        {player && player.health}
-                    </p>
-                </div>
-
-                <div
-                    css={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        marginBlockEnd: "5px",
-                    }}
-                >
-                    <p
-                        css={{
-                            marginBlock: 0,
-                            color: Colors.Text.danger,
-                        }}
-                    >
-                        Debt: {player && formatMoney(player.debt)}{" "}
-                        {player.debt > 0 && (
-                            <span css={{ color: Colors.Text.subtle }}>
-                                (5%)
-                            </span>
-                        )}
-                    </p>
-
-                    {player.debt > 0 &&
-                        canAfford &&
-                        (isLoading ? (
-                            <p
-                                css={{
-                                    marginBlock: 0,
-                                    color: Colors.Text.subtle,
-                                }}
-                            >
-                                Paying...
-                            </p>
-                        ) : (
-                            <ButtonPrimary
-                                type={"Sized"}
-                                label={"Pay Debt"}
-                                border={"None"}
-                                scheme={"Inverse"}
-                                isLoading={isLoading}
-                                clickCB={handlePayDebtClick}
-                            />
-                        ))}
-                </div>
-            </div>
-            <div
-                css={{
+                    blockSize: "calc(100% - 70px)",
                     inlineSize: "100%",
                     display: "flex",
                     flexDirection: "column",
-                    alignItems: "flex-start",
-                    paddingBlock,
-                    paddingInline,
-                    borderColor: "transparent",
-                    borderBlockEndColor: Colors.Border.subtle,
-                    borderRadius: "1px",
-                    borderStyle: "solid",
-                    borderWidth: "1px",
-
-                    "& button": {
-                        marginBlockStart: layout === "full" ? "-10px" : "-2px",
+                    alignItems: "center",
+                    justifyContent: "space-evenly",
+                    paddingInline: containerPaddingInline,
+                    "& p": {
+                        fontFamily: "Share Tech Mono",
+                        wordSpacing: 0,
                     },
                 }}
             >
-                <p css={{ marginBlockStart: 0, marginBlockEnd: "15px" }}>
-                    Pack Type: Backpack
-                </p>
+                <div
+                    css={{
+                        inlineSize: "100%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        paddingBlock,
+                        paddingInline,
+                        borderColor: "transparent",
+                        borderBlockEndColor: Colors.Border.subtle,
+                        borderRadius: "1px",
+                        borderStyle: "solid",
+                        borderWidth: "1px",
+
+                        "& button": {
+                            marginBlockStart:
+                                layout === "full" ? "-10px" : "-2px",
+                        },
+                    }}
+                >
+                    <div>
+                        <p css={{ marginBlockStart: 0, marginBlockEnd: "5px" }}>
+                            <span css={{ marginInlineEnd: "5px" }}>
+                                <FontAwesomeIcon icon={faClock} />
+                            </span>
+                            {turnsLeft && getTimeLeft(turnsLeft)}
+                        </p>
+                        <p css={{ marginBlock: 0, color: Colors.Text.subtle }}>
+                            {turnsLeft} hours left
+                        </p>
+                    </div>
+                    <ButtonPrimary
+                        type={"Sized"}
+                        label={"End Game"}
+                        scheme={"Inverse"}
+                        border={"None"}
+                        isDanger={true}
+                        clickCB={handleConfirmEndGame}
+                    />
+                </div>
 
                 <div
                     css={{
                         inlineSize: "100%",
-                        display: "grid",
-                        gridTemplateColumns: "1fr 1fr",
+                        paddingBlock,
+                        paddingInline,
+                        borderColor: "transparent",
+                        borderBlockEndColor: Colors.Border.subtle,
+                        borderRadius: "1px",
+                        borderStyle: "solid",
+                        borderWidth: "1px",
                     }}
                 >
-                    {sortedPack.map(([cut, amount], idx) => (
+                    <div
+                        css={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            marginBlockEnd: "5px",
+                        }}
+                    >
+                        <p css={{ marginBlockStart: 0, marginBlockEnd: "5px" }}>
+                            {player && formatMoney(player.funds)}
+                        </p>
+
+                        <p css={{ marginBlock: 0 }}>
+                            <span css={{ marginInlineEnd: "5px" }}>
+                                <FontAwesomeIcon icon={faHeart} fontSize={16} />
+                            </span>
+                            {player && player.health}
+                        </p>
+                    </div>
+
+                    <div
+                        css={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                            marginBlockEnd: "5px",
+                        }}
+                    >
                         <p
-                            key={`${cut}-${idx}`}
                             css={{
-                                marginBlockStart: 0,
-                                marginBlockEnd: "5px",
-                                color:
-                                    amount > 0
-                                        ? Colors.Text.base
-                                        : Colors.Text.disable,
-                                fontStyle: amount > 0 ? "normal" : "italic",
-                                textTransform: "capitalize",
+                                marginBlock: 0,
+                                color: Colors.Text.danger,
                             }}
                         >
-                            {cut}: {amount > 0 ? amount : "--"}
+                            Debt: {player && formatMoney(player.debt)}{" "}
+                            {player.debt > 0 && (
+                                <span css={{ color: Colors.Text.subtle }}>
+                                    (5%)
+                                </span>
+                            )}
                         </p>
-                    ))}
+
+                        {player.debt > 0 &&
+                            canAfford &&
+                            (isLoading ? (
+                                <p
+                                    css={{
+                                        marginBlock: 0,
+                                        color: Colors.Text.subtle,
+                                    }}
+                                >
+                                    Paying...
+                                </p>
+                            ) : (
+                                <ButtonPrimary
+                                    type={"Sized"}
+                                    label={"Pay Debt"}
+                                    border={"None"}
+                                    scheme={"Inverse"}
+                                    isLoading={isLoading}
+                                    clickCB={handlePayDebtClick}
+                                />
+                            ))}
+                    </div>
+                </div>
+                <div
+                    css={{
+                        inlineSize: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                        paddingBlock,
+                        paddingInline,
+                        borderColor: "transparent",
+                        borderBlockEndColor: Colors.Border.subtle,
+                        borderRadius: "1px",
+                        borderStyle: "solid",
+                        borderWidth: "1px",
+
+                        "& button": {
+                            marginBlockStart:
+                                layout === "full" ? "-10px" : "-2px",
+                        },
+                    }}
+                >
+                    <p css={{ marginBlockStart: 0, marginBlockEnd: "15px" }}>
+                        Pack Type: Backpack
+                    </p>
+
+                    <div
+                        css={{
+                            inlineSize: "100%",
+                            display: "grid",
+                            gridTemplateColumns: "1fr 1fr",
+                        }}
+                    >
+                        {sortedPack.map(([cut, amount], idx) => (
+                            <p
+                                key={`${cut}-${idx}`}
+                                css={{
+                                    marginBlockStart: 0,
+                                    marginBlockEnd: "5px",
+                                    color:
+                                        amount > 0
+                                            ? Colors.Text.base
+                                            : Colors.Text.disable,
+                                    fontStyle: amount > 0 ? "normal" : "italic",
+                                    textTransform: "capitalize",
+                                }}
+                            >
+                                {cut}: {amount > 0 ? amount : "--"}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+
+                <div
+                    css={{
+                        paddingBlock,
+                        paddingInline,
+                    }}
+                >
+                    <h4
+                        css={{
+                            marginBlockStart: 0,
+                            marginBlockEnd: "15px",
+                            fontFamily: "Michroma",
+                            fontVariantCaps: "small-caps",
+                            textTransform: "capitalize",
+                        }}
+                    >
+                        {station?.name}
+                    </h4>
+
+                    <p
+                        css={{
+                            marginBlockStart: 0,
+                            marginBlockEnd: "5px",
+                        }}
+                    >
+                        Gang: {station?.gangName}
+                    </p>
+
+                    <p
+                        css={{
+                            marginBlockStart: 0,
+                            marginBlockEnd: "5px",
+                        }}
+                    >
+                        Street Rep: Hunted
+                    </p>
+                    <small css={{ marginBlock: 0, color: Colors.Text.subtle }}>
+                        Too many stick-ups has drawn the attention of the
+                        locals. Watch your back.
+                    </small>
                 </div>
             </div>
 
-            <div
-                css={{
-                    paddingBlock,
-                    paddingInline,
-                }}
-            >
-                <h4
-                    css={{
-                        marginBlockStart: 0,
-                        marginBlockEnd: "15px",
-                        fontFamily: "Michroma",
-                        fontVariantCaps: "small-caps",
-                        textTransform: "capitalize",
-                    }}
-                >
-                    {station?.name}
-                </h4>
-
-                <p
-                    css={{
-                        marginBlockStart: 0,
-                        marginBlockEnd: "5px",
-                    }}
-                >
-                    Gang: {station?.gangName}
-                </p>
-
-                <p
-                    css={{
-                        marginBlockStart: 0,
-                        marginBlockEnd: "5px",
-                    }}
-                >
-                    Street Rep: Hunted
-                </p>
-                <small css={{ marginBlock: 0, color: Colors.Text.subtle }}>
-                    Too many stick-ups has drawn the attention of the locals.
-                    Watch your back.
-                </small>
-            </div>
-        </div>
+            {isModalOpen && <EndModal onCloseModal={handleCancelEndGame} />}
+        </Fragment>
     );
 };
