b17196f5b60fbf64b91fe6ebc48c8261a94e2258
Jon <jonchoukroun@gmail.com>
2021-12-27 23:06:43 -0500

    Update types

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,32 +5,20 @@ type Market = { [key in CutName]: { price: number; quantity: number } };
 type Pack = { [key in CutName]: number };
 
 type PackName = "mini_fridge" | "shopping_cart" | "suitcase" | "wheelbarrow";
-// type PackListings = {
-//     [key in PackName]: { pack_space: number; price: number };
-// };
 
 type PackListing = [string, { pack_space: number; price: number }][];
 
-// type WeaponName =
-//     | "box_cutter"
-//     | "brass_knuckes"
-//     | "hedge_clippers"
-//     | "hockey_stick"
-//     | "knife"
-//     | "machete";
-// type WeaponListings = {
-//     [key in WeaponName]: { cuts: CutName[]; damage: number; price: number };
-// };
+type WeaponName =
+    | "box_cutter"
+    | "brass_knuckles"
+    | "hedge_clippers"
+    | "hockey_stick"
+    | "machete";
 
-// type Store = {
-//     [key in WeaponName | PackName]: key extends PackName
-//         ? { pack_space: number; price: number }
-//         : {
-//               cuts: string[];
-//               damage: number;
-//               price: number;
-//           };
-// };
+type WeaponListing = [
+    string,
+    { cuts: string[]; damage: number; price: number },
+][];
 
 type Store = Record<
     string,
