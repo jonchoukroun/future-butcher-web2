7d3fbde0151157d3dba8decc203065dc1799e489
Jon <jonchoukroun@gmail.com>
2022-07-19 08:53:54 -0700

    Define pale amber for disabled text

--- src/Styles/colors.tsx
+++ src/Styles/colors.tsx
@@ -1,15 +1,15 @@
 /** Amber-based palette */
 const AMBER = "#ffb000";
 const LIGHT_AMBER = "rgba(255, 176, 0, 0.75)";
+const PALE_AMBER = "rgba(255, 176, 0, 0.3)";
 const RED = "rgba(255, 51, 51, 1)";
 const BLACK = "#171717";
 
 export const Text = {
     base: AMBER,
     subtle: LIGHT_AMBER,
-    disable: LIGHT_AMBER,
+    disable: PALE_AMBER,
     inverse: BLACK,
-    accent: AMBER,
     danger: RED,
 };
 
