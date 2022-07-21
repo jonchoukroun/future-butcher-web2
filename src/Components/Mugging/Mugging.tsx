2cf5de1fda610ff4a3798b8a5da64b6711e31013
Jon <jonchoukroun@gmail.com>
2022-07-20 23:02:25 -0700

    Redesign base mugging screen

--- src/Components/Mugging/Mugging.tsx
+++ src/Components/Mugging/Mugging.tsx
@@ -5,9 +5,10 @@ import { unstable_batchedUpdates } from "react-dom";
 
 import { MuggingDefeat } from "./MuggingDefeat";
 import { MuggingVictory } from "./MuggingVictory";
-import { ButtonPrimary } from "../Form/ButtonPrimary";
+import { Button, ButtonScheme, ButtonSize } from "../Form";
 import { useGameState, Screen } from "../../GameData/GameStateProvider";
 import { useChannel, Callback } from "../../PhoenixChannel/ChannelProvider";
+import * as Animations from "../../Styles/animations";
 import * as Colors from "../../Styles/colors";
 
 export const Mugging = () => {
@@ -22,6 +23,7 @@ export const Mugging = () => {
         player === undefined ||
         turnsLeft === undefined
     ) {
+        console.log("!!Mugging", muggers, pack, player, turnsLeft);
         throw new Error("State is undefined");
     }
 
@@ -32,14 +34,7 @@ export const Mugging = () => {
         "victory" | "defeat" | undefined
     >(undefined);
 
-    console.log("!!muggers", muggers);
     const currentMuggerRef = useRef(muggers[0]);
-    // const currentMuggerRef = useRef<string>();
-    // useEffect(() => {
-    //     const [currentMugger] = muggers;
-    //     currentMuggerRef.current = currentMugger;
-    //     console.log("!!currentMuggerRef", currentMuggerRef.current);
-    // }, []);
 
     const { handlePushCallback } = useChannel();
     const [isLoading, setIsLoading] = useState(false);
@@ -58,10 +53,10 @@ export const Mugging = () => {
                 initialTurnsLeft.current > response.rules.turns_left
                     ? "defeat"
                     : "victory";
+
             // If the mugger is defeated, remove him from the list
             if (outcome === "victory") {
                 dispatch({ type: "killMugger" });
-                console.log("!!", muggers, currentMuggerRef.current);
             }
             setMuggingState(outcome);
             setIsLoading(false);
@@ -75,64 +70,91 @@ export const Mugging = () => {
                 inlineSize: "100%",
                 display: "flex",
                 flexDirection: "column",
+                paddingInline: "10px",
             }}
         >
-            <h2
-                css={{
-                    marginBlock: "20px",
-                    color: Colors.Text.danger,
-                    textTransform: "uppercase",
-                    letterSpacing: "2px",
-                    wordSpacing: "4px",
-                    textAlign: "center",
-                }}
-            >
-                {muggingState === undefined
-                    ? "You're under attack!"
-                    : muggingState === "defeat"
-                    ? "You lost!"
-                    : "You win!"}
-            </h2>
+            <div css={{ display: "flex", marginBlock: "20px" }}>
+                <h4 css={{ marginInlineEnd: "10px", opacity: 0 }}>{">"}</h4>
+                <h2
+                    css={{
+                        marginBlock: "20px",
+                        marginInlineStart: "6px",
+                        color: Colors.Text.danger,
+                    }}
+                >
+                    {muggingState === undefined
+                        ? "You're under attack!"
+                        : muggingState === "defeat"
+                        ? "You lost!"
+                        : "You win!"}
+                </h2>
+            </div>
+
             {muggingState === "victory" ? (
                 <MuggingVictory initialPack={initialPack.current} />
             ) : muggingState === "defeat" ? (
                 <MuggingDefeat initialTurnsLeft={initialTurnsLeft.current} />
             ) : (
-                <div
-                    css={{
-                        display: "flex",
-                        flexDirection: "column",
-                        paddingInline: "10px",
-                    }}
-                >
-                    <p>
-                        One of the city&apos;s relentless muggers,{" "}
-                        {currentMuggerRef.current}, follows you from the subway.
-                    </p>
-                    <p>
-                        Pulling a well-used blade, he charges you. You have a
-                        split second to react.{" "}
-                    </p>
+                <div css={{ paddingInline: "10px" }}>
+                    <div
+                        css={{
+                            display: "flex",
+                        }}
+                    >
+                        <h4 css={{ marginInlineEnd: "10px" }}>{">"}</h4>
+                        <h4 css={{ marginInlineStart: "6px" }}>
+                            One of the city&apos;s relentless muggers,{" "}
+                            {currentMuggerRef.current}, follows you from the
+                            subway.
+                        </h4>
+                    </div>
 
                     <div
                         css={{
                             display: "flex",
-                            justifyContent: "center",
-                            marginBlockStart: "20px",
                         }}
                     >
-                        <ButtonPrimary
-                            type={"Full"}
-                            label={"Fight"}
-                            isDanger={true}
-                            isLoading={isLoading}
-                            clickCB={handleFightMuggerClick}
-                        />
+                        <h4 css={{ marginInlineEnd: "10px" }}>{">"}</h4>
+                        <h4 css={{ marginInlineStart: "6px" }}>
+                            Pulling a well-used blade, he charges you. You have
+                            a split second to react.
+                        </h4>
                     </div>
 
                     {!player.weapon && (
-                        <p>A weapon would have come in handy here.</p>
+                        <div
+                            css={{
+                                display: "flex",
+                            }}
+                        >
+                            <h4 css={{ marginInlineEnd: "10px" }}>{">"}</h4>
+                            <h4 css={{ marginInlineStart: "6px" }}>
+                                A weapon would have come in handy here.
+                            </h4>
+                        </div>
                     )}
+
+                    <div
+                        css={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <h4
+                            css={{
+                                marginInlineEnd: "10px",
+                                animation: `${Animations.blink} 1s linear infinite`,
+                            }}
+                        >
+                            {">"}
+                        </h4>
+                        <Button
+                            size={ButtonSize.Compact}
+                            scheme={ButtonScheme.Inverse}
+                            label={"Fight the Mugger"}
+                            clickCB={handleFightMuggerClick}
+                        />
+                    </div>
                 </div>
             )}
         </div>
