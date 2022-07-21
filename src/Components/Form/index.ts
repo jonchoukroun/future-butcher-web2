66e998e3af979cc1852c7b253412487cfe860e08
Jon <jonchoukroun@gmail.com>
2022-07-21 08:48:33 -0700

    Restructure components and screens. Add empty loc directory

--- src/Components/Form/index.ts
+++
@@ -1,5 +0,0 @@
-import { Button, ButtonScheme, ButtonSize } from "./Button";
-import { ButtonPrimary } from "./ButtonPrimary";
-import { TextInput } from "./TextInput";
-
-export { Button, ButtonScheme, ButtonSize, ButtonPrimary, TextInput };
--- src/Components/Utils/formatMoney.ts
+++
@@ -1,7 +0,0 @@
-export function formatMoney(amount: number): string {
-    return new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 0,
-    }).format(amount);
-}
