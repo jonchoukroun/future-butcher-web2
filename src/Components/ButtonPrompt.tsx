066e3b2076ea2c33fa9d9925d01e38d134eb1644
Jon <jonchoukroun@gmail.com>
2022-07-21 13:02:50 -0700

    Define button with prompt

---
+++ src/Components/ButtonPrompt.tsx
@@ -0,0 +1,104 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
+
+import * as Animations from "../Styles/animations";
+import * as Colors from "../Styles/colors";
+
+export const enum ButtonPromptSize {
+    Compact,
+    Full,
+}
+type ButtonPromptSizeType = typeof ButtonPromptSize[keyof typeof ButtonPromptSize];
+
+interface ButtonPromptProps {
+    label: string;
+    size: ButtonPromptSizeType;
+    blink?: boolean;
+    disabled?: boolean;
+    hidden?: boolean;
+    loading?: boolean;
+    clickCB: () => void;
+}
+
+export function ButtonPrompt({
+    label,
+    size,
+    blink,
+    disabled,
+    hidden,
+    loading,
+    clickCB,
+}: ButtonPromptProps) {
+    const stylesForSize = getStyleForSize(size);
+
+    const backgroundColor = Colors.Background.base;
+    const backgroundColorActive = Colors.Background.inverse;
+    const color = disabled ? Colors.Text.disable : Colors.Text.base;
+    const colorActive = Colors.Text.inverse;
+
+    return (
+        <button
+            css={css(stylesForSize, {
+                backgroundColor,
+                color,
+                border: 0,
+                opacity: hidden ? 0 : 1,
+                "&:active": {
+                    backgroundColor: backgroundColorActive,
+                    color: colorActive,
+                },
+            })}
+            disabled={disabled || hidden}
+            onClick={clickCB}
+        >
+            <span
+                css={{
+                    marginInlineEnd: "18px",
+                    animation: blink
+                        ? `${Animations.blink} 1s linear infinite`
+                        : 0,
+                }}
+            >
+                {">"}
+            </span>
+            {loading ? "..." : label}
+        </button>
+    );
+}
+
+function getStyleForSize(size: ButtonPromptSize) {
+    const baseButtonStyles = css({
+        margin: 0,
+        fontFamily: "'Courier New', Courier, monospace",
+        fontSize: "20px",
+        fontWeight: 700,
+        lineHeight: "26px",
+        textTransform: "uppercase",
+    });
+
+    switch (size) {
+        case ButtonPromptSize.Compact:
+            return css(
+                {
+                    blockSize: "auto",
+                    inlineSize: "auto",
+                    paddingBlock: "5px",
+                },
+                baseButtonStyles,
+            );
+
+        case ButtonPromptSize.Full:
+            return css(
+                {
+                    blockSize: "46px",
+                    inlineSize: "100%",
+                    padding: 0,
+                    textAlign: "start",
+                },
+                baseButtonStyles,
+            );
+
+        default:
+            return baseButtonStyles;
+    }
+}
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,5 +1,16 @@
 import { Button, ButtonScheme, ButtonSize } from "./Button";
+import { ButtonPrompt, ButtonPromptSize } from "./ButtonPrompt";
 import { ButtonPrimary } from "./ButtonPrimary";
+import { PrintLine } from "./PrintLine";
 import { TextInput } from "./TextInput";
 
-export { Button, ButtonScheme, ButtonSize, ButtonPrimary, TextInput };
+export {
+    Button,
+    ButtonScheme,
+    ButtonSize,
+    ButtonPrimary,
+    ButtonPrompt,
+    ButtonPromptSize,
+    PrintLine,
+    TextInput,
+};
