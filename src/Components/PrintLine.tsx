bc74b9b7736b1902e0f9811649b5829a2f15993b
Jon <jonchoukroun@gmail.com>
2022-07-23 15:25:42 -0700

    Allow line sizing

--- src/Components/PrintLine.tsx
+++ src/Components/PrintLine.tsx
@@ -4,22 +4,34 @@ import { jsx } from "@emotion/react";
 import { Prompt } from "./Prompt";
 import * as Colors from "../Styles/colors";
 
+export const enum LineSize {
+    Body,
+    Title,
+}
+type LineSizeType = typeof LineSize[keyof typeof LineSize];
+
 interface PrintLineProps {
     text: string;
+    size: LineSizeType;
+    showPrompt?: boolean;
 }
 
-export function PrintLine({ text }: PrintLineProps) {
+export function PrintLine({
+    text,
+    size = LineSize.Body,
+    showPrompt = true,
+}: PrintLineProps) {
     return (
         <div
             css={{
                 display: "flex",
-                alignItems: "flex-start",
+                alignItems: size === LineSize.Title ? "center" : "flex-start",
                 marginBlockEnd: "8px",
-                "& h4": { color: Colors.Text.base, marginBlock: 0 },
+                "& h1, h4": { color: Colors.Text.base, marginBlock: 0 },
             }}
         >
-            <Prompt />
-            <h4>{text}</h4>
+            <Prompt hidden={!showPrompt} />
+            {size === LineSize.Title ? <h1>{text}</h1> : <h4>{text}</h4>}
         </div>
     );
 }
