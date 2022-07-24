c2683dfcd251132fd54f94e8a6f89e56cb69c5c5
Jon <jonchoukroun@gmail.com>
2022-07-24 14:05:37 -0700

    Allow setting prompt symbol and color

--- src/Components/Prompt.tsx
+++ src/Components/Prompt.tsx
@@ -2,24 +2,34 @@
 import { jsx } from "@emotion/react";
 
 import * as Animations from "../Styles/animations";
+import * as Colors from "../Styles/colors";
 
 interface PromptProps {
+    symbol?: string;
+    color?: string;
     blink?: boolean;
     hidden?: boolean;
 }
 
-export function Prompt({ blink, hidden = false }: PromptProps) {
+export function Prompt({
+    symbol = ">",
+    color = Colors.Text.base,
+    blink = false,
+    hidden = false,
+}: PromptProps) {
     return (
         <h4
             css={{
                 display: "inline",
+                marginBlock: 0,
                 marginInlineEnd: "18px",
                 fontSize: "16px",
+                color,
                 animation: blink ? `${Animations.blink} 1s linear infinite` : 0,
                 opacity: hidden ? 0 : 1,
             }}
         >
-            {">"}
+            {symbol}
         </h4>
     );
 }
