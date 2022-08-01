568e3a9ef91184cfc151f6e21b1dba82360c4c80
Jon <jonchoukroun@gmail.com>
2022-07-31 17:49:37 -0700

    Redesign mugging intro and defeat screens

--- src/Screens/Mugging/Mugging.tsx
+++ src/Screens/Mugging/Mugging.tsx
@@ -5,15 +5,12 @@ import { unstable_batchedUpdates } from "react-dom";
 
 import { MuggingDefeat } from "./MuggingDefeat";
 import { MuggingVictory } from "./MuggingVictory";
-import { ButtonPrompt, ButtonPromptSize } from "../../Components";
+import { ScreenTemplate } from "../../Components";
 import { Screen } from "../../GameData";
 import { useGameState } from "../../GameData/GameStateProvider";
 import { isApiError } from "../../GameData/State";
 import { handleMessage, MessageLevel } from "../../Logging/handleMessage";
 import { useChannel } from "../../PhoenixChannel/ChannelProvider";
-
-import * as Animations from "../../Styles/animations";
-import * as Colors from "../../Styles/colors";
 
 export const Mugging = () => {
     const {
@@ -22,7 +19,6 @@ export const Mugging = () => {
     } = useGameState();
 
     if (player === undefined || turnsLeft === undefined) {
-        console.log("!!Mugging", muggers, player, turnsLeft);
         throw new Error("State is undefined");
     }
 
@@ -30,20 +26,20 @@ export const Mugging = () => {
         handleMessage("Muggers is undefined", MessageLevel.Error);
     }
 
+    const { handlePushCallback } = useChannel();
+
     const { pack } = player;
+
     const initialTurnsLeft = useRef(turnsLeft);
     const initialPack = useRef(pack);
-
-    const [muggingState, setMuggingState] = useState<
-        "victory" | "defeat" | undefined
-    >(undefined);
-
     const currentMuggerRef = useRef(
         muggers === undefined ? "Fred Savage" : muggers[0],
     );
-
-    const { handlePushCallback } = useChannel();
+    const [muggingState, setMuggingState] = useState<
+        "victory" | "defeat" | undefined
+    >(undefined);
     const [isLoading, setIsLoading] = useState(false);
+
     const handleFightMuggerClick = async () => {
         if (isLoading) return;
         setIsLoading(true);
@@ -70,99 +66,30 @@ export const Mugging = () => {
         });
     };
 
+    if (muggingState === "defeat") {
+        return <MuggingDefeat initialTurnsLeft={initialTurnsLeft.current} />;
+    }
+
+    if (muggingState === "victory") {
+        return <MuggingVictory initialPack={initialPack.current} />;
+    }
+
+    const muggingContent = [
+        `One of the city's relentless muggers, ${currentMuggerRef.current}, follows you from the subway.`,
+        "Pulling a well-used blade, they charge you!",
+        "You have a split second to react...",
+    ];
+    if (!player.weapon)
+        muggingContent.push("...a weapon may have come in handy here...");
+
     return (
-        <div
-            css={{
-                blockSize: "100%",
-                inlineSize: "100%",
-                display: "flex",
-                flexDirection: "column",
-                paddingInline: "10px",
-            }}
-        >
-            <div css={{ display: "flex", marginBlock: "20px" }}>
-                <h4 css={{ marginInlineEnd: "10px", opacity: 0 }}>{">"}</h4>
-                <h2
-                    css={{
-                        marginBlock: "20px",
-                        marginInlineStart: "6px",
-                        color: Colors.Text.danger,
-                    }}
-                >
-                    {muggingState === undefined
-                        ? "You're under attack!"
-                        : muggingState === "defeat"
-                        ? "You lost!"
-                        : "You win!"}
-                </h2>
-            </div>
-
-            {muggingState === "victory" ? (
-                <MuggingVictory initialPack={initialPack.current} />
-            ) : muggingState === "defeat" ? (
-                <MuggingDefeat initialTurnsLeft={initialTurnsLeft.current} />
-            ) : (
-                <div css={{ paddingInline: "10px" }}>
-                    <div
-                        css={{
-                            display: "flex",
-                        }}
-                    >
-                        <h4 css={{ marginInlineEnd: "10px" }}>{">"}</h4>
-                        <h4 css={{ marginInlineStart: "6px" }}>
-                            One of the city&apos;s relentless muggers,{" "}
-                            {currentMuggerRef.current}, follows you from the
-                            subway.
-                        </h4>
-                    </div>
-
-                    <div
-                        css={{
-                            display: "flex",
-                        }}
-                    >
-                        <h4 css={{ marginInlineEnd: "10px" }}>{">"}</h4>
-                        <h4 css={{ marginInlineStart: "6px" }}>
-                            Pulling a well-used blade, he charges you. You have
-                            a split second to react.
-                        </h4>
-                    </div>
-
-                    {!player.weapon && (
-                        <div
-                            css={{
-                                display: "flex",
-                            }}
-                        >
-                            <h4 css={{ marginInlineEnd: "10px" }}>{">"}</h4>
-                            <h4 css={{ marginInlineStart: "6px" }}>
-                                A weapon would have come in handy here.
-                            </h4>
-                        </div>
-                    )}
-
-                    <div
-                        css={{
-                            display: "flex",
-                            alignItems: "center",
-                        }}
-                    >
-                        <h4
-                            css={{
-                                marginInlineEnd: "10px",
-                                animation: `${Animations.blink} 1s linear infinite`,
-                            }}
-                        >
-                            {">"}
-                        </h4>
-                        <ButtonPrompt
-                            size={ButtonPromptSize.Compact}
-                            label={"Fight the Mugger"}
-                            clickCB={handleFightMuggerClick}
-                        />
-                    </div>
-                </div>
-            )}
-        </div>
+        <ScreenTemplate
+            title={"A Mugger Attacks!"}
+            content={muggingContent}
+            danger={true}
+            buttonLabel={"Fight the mugger!"}
+            isLoading={isLoading}
+            clickCB={handleFightMuggerClick}
+        />
     );
 };
--- src/Screens/Mugging/MuggingDefeat.tsx
+++ src/Screens/Mugging/MuggingDefeat.tsx
@@ -1,6 +1,8 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
 
+import { ScreenTemplate } from "../../Components";
+import { Screen } from "../../GameData";
 import { useGameState } from "../../GameData/GameStateProvider";
 
 export const MuggingDefeat = ({
@@ -14,27 +16,32 @@ export const MuggingDefeat = ({
     } = useGameState();
     if (turnsLeft === undefined) throw new Error("State is undefined");
 
+    const handleClick = () => {
+        dispatch({ type: "changeScreen", screen: Screen.Market });
+    };
+
     const hoursLost = initialTurnsLeft - turnsLeft;
 
+    const muggingContent = [
+        "The mugger kicked your ass!",
+        `You lay unconscious in the gutter for ${hoursLost} ${pluralize(
+            hoursLost,
+        )} while local kids threw rocks at your sorry ass.`,
+        "Next time, bring a better weapon to the fight.",
+    ];
+
     return (
-        <div
-            css={{
-                display: "flex",
-                flexDirection: "column",
-                paddingInline: "10px",
-            }}
-        >
-            <p>
-                The mugger kicked your ass. You were out for {hoursLost}{" "}
-                {hoursLost > 1 ? "hours" : "hour"}. Better get back out there.
-            </p>
-            <div
-                css={{
-                    display: "flex",
-                    justifyContent: "center",
-                    marginBlockStart: "20px",
-                }}
-            ></div>
-        </div>
+        <ScreenTemplate
+            title={"You lose"}
+            danger={true}
+            content={muggingContent}
+            isLoading={false}
+            buttonLabel={"Get back to work"}
+            clickCB={handleClick}
+        />
     );
 };
+
+function pluralize(time: number): string {
+    return time === 1 ? "hour" : "hours";
+}
