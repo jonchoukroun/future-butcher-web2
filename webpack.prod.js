555c543458ec4dd9762c1e6498cd62f4a3e94b04
Jon <jonchoukroun@gmail.com>
2022-08-10 17:29:27 -0700

    Remove API url from production webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const { DefinePlugin } = require("webpack");
 
 module.exports = merge(common, {
     mode: "production",
     devtool: "source-map",
-    plugins: [
-        new DefinePlugin({
-            "process.env": {
-                API_URL: JSON.stringify("wss://api.futurebutcher.com/socket"),
-            },
-        }),
-    ],
 });
