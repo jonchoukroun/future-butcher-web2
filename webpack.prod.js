01d2abb4ba6a61f5866866d396f54733f463ea3f
Jon <jonchoukroun@gmail.com>
2022-08-10 17:36:23 -0700

    Restore process env definition to prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const { DefinePlugin } = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "production",
     devtool: "source-map",
+    plugins: [
+        new DefinePlugin({
+            "process.env": {},
+        }),
+    ],
 });
