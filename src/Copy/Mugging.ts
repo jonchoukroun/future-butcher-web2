980cd4db256f509eb5a7789f050d7230a6c9e482
Jon <jonchoukroun@gmail.com>
2022-08-29 19:43:16 -0700

    Fix mugging messaging

--- src/Copy/Mugging.ts
+++ src/Copy/Mugging.ts
@@ -1,6 +1,6 @@
 const muggingRunVictory = [
     "Who's faster than you? Nobody. Not the mugger you left wheezing in the dust.",
-    "You sprint like death is chasing you. Considering the rabid mugger and his rusty blade, who can blame you?",
+    "You sprint like death is chasing you. Considering the rabid mugger and their rusty blade, who can blame you?",
 ];
 const muggingFightVictory = [
     "Neighborhood kids cheer as you kick the mugger's ass.",
@@ -21,7 +21,7 @@ export function getDefeatCopy(hoursLost: number, damageTaken: number) {
         `You lay unconscious in the gutter for ${hoursLost} ${pluralize(
             hoursLost,
         )} while local kids threw rocks at your sorry ass.`,
-        `They didn't care for your attempt to fight. He gave you the ass kicking of a lifetime and left you bleeding in the street for ${hoursLost} ${pluralize(
+        `They gave you the ass kicking of a lifetime and left you bleeding in the street for ${hoursLost} ${pluralize(
             hoursLost,
         )}.`,
         ` You were out cold for ${hoursLost} ${pluralize(hoursLost)}.`,
