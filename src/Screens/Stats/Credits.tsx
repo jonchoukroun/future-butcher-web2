c04bb10c4d424c1817272d48b1f9715496e34be6
Jon <jonchoukroun@gmail.com>
2022-09-04 22:42:15 -0700

    Fix full layout bug on returning from credits

--- src/Screens/Stats/Credits.tsx
+++ src/Screens/Stats/Credits.tsx
@@ -2,12 +2,19 @@
 import { jsx } from "@emotion/react";
 
 import { ScreenTemplate } from "../../Components";
+import { useWindowSize } from "../../Components/Window/WindowSizeProvider";
+import { Screen } from "../../GameData";
 import { useGameState } from "../../GameData/GameStateProvider";
 
 export function Credits() {
     const { dispatch } = useGameState();
+    const { layout } = useWindowSize();
     const handleBackClick = () => {
-        dispatch({ type: "changeScreen", screen: "stats" });
+        if (layout === "full") {
+            dispatch({ type: "changeScreen", screen: Screen.Market });
+        } else {
+            dispatch({ type: "changeScreen", screen: Screen.Stats });
+        }
     };
     return (
         <ScreenTemplate
@@ -18,7 +25,7 @@ export function Credits() {
                 "Top players of all time: Doh & Hannah",
                 "Future Butcher was designed and devloped by Jon Choukroun. The game runs on Elixir/Phoenix and React.",
                 "Copyright 2022",
-                'Inspired by the John E. Dell classic "Drug Wars"',
+                'Inspired by the John E. Dell classic "Drug Wars."',
             ]}
             primaryButtonLabel={"Back"}
             primaryIsLoading={false}
